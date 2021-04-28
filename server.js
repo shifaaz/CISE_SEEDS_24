b1a0417c08c3620e5fc6f829a8854c6cac02127c
Shahil <shkhan078@gmail.com>
2021-04-28 21:52:53 +1200

    Changing to see if deployment works with heroku

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config({ path: "./config.env" });
+const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const postRoutes = require("./routes/postRoutes");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/posts", postRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("API running");
+    });
+}
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
