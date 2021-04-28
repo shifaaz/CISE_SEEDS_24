1a4649ee1e03aec524450abe7d34d286daaada89
Shahil <shkhan078@gmail.com>
2021-04-28 22:38:38 +1200

    Trying to sort deployment error with dependencies

--- client/src/Table.js
+++ client/src/Table.js
@@ -3,7 +3,7 @@ import { useTable, useFilters, useSortBy } from "react-table";
 
 export default function Table({ columns, data }) {
     const [filterInput, setFilterInput] = useState("");
-    // Use the state and functions returned from useTable to build your UI
+
     const {
         getTableProps,
         getTableBodyProps,
