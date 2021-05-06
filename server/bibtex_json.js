e176d845f03dde555a65ed8fc36d6647f332f767
Shahil <shkhan078@gmail.com>
2021-05-07 00:39:17 +1200

    Deleted files as issues with connectivity and redundancy of code

--- server/bibtex_json.js
+++
@@ -1,6 +0,0 @@
-const bibtexParse = require('@orcid/bibtex-parse-js');
-
-
-var sample = bibtexParse.toJSON('@proceedings{Anonymous:O7UPDeq-,title={A prototype empirical evaluation of test driven development - Software Metrics, 2004. Proceedings. 10th International Symposium on}, year={2001}, month={aug}}');
-
-console.log(sample);
