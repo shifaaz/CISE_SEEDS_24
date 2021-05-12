c14250fdb26edd44e7ed5d9bad5af7eaae2c1f6f
penzen <penzenlama@gmail.com>
2021-05-12 18:16:59 +1200

    Created funtions for quick search dates

--- Date_range/dater.js
+++ Date_range/dater.js
@@ -1,44 +1,69 @@
-const Transaction = require('../models/fileSchema');
+const articleSchema = require('../models/articleSchema');
+const Transaction = require('../models/articleSchema'); 
 // check if the routing is correct. 
+// needs to go in the controller
+
+//Integreate this with the others 
+exports.sortbyDate = async (req, res) => { //exports the information  
+    // needs to be imported in the sorting part. 
 
 
-exports.getTransactionByDate = async(req, res) => {
+    //get dates from req.query by es6 object destructuring
 
-
-   //get dates from req.query by es6 object destructuring
-
-    let{ startDate, endDate } = req.query;
+    let { startDate, endDate } = req.query; 
     // the information is both in by typing. 
 
-  //1. check that date is not empty
-   if(startDate === '' || endDate === '') {
-   return res.status(400).json({
-       status:'failure',
-       message: 'Please ensure you pick two dates'
+    //1. check that date is not empty
+    if (startDate === '' || endDate === '') {
+        return res.status(400).json({
+            status: 'failure',
+            message: 'Please ensure you pick two dates'
         })
-       }
+    }
 
-  //2. check that date is in the right format
- //expected result: YYY-MMM-DDD
- 
- //only the year required. 
-   //console.log({ startDate, endDate});
+    //2. check that date is in the right format
+    //expected result: YYY-MMM-DDD
 
-
-//In some cases you'll get a date-time format where you have to separate the date
-//from the time.
+    //only the year required. 
+    //console.log({ startDate, endDate});
 
 
-//3. Query database using Mongoose
-//Mind the curly braces
-const transactions = Transaction.find({ 
-  date: {
-        $gte: new Date(new Date(startDate)), // greter than operator 
-        $lt: new Date(new Date(endDate)) // Less than operator
-         }
-  }).sort({ date: 'asc'})  // sorted in ascending order
+    //In some cases you'll get a date-time format where you have to separate the date
+    //from the time.
+
+
+    //3. Query database using Mongoose
+    //Mind the curly braces
+    const  genral_search = articleSchema.find({ // 
+        date: {
+            $gte: new Date(new Date(startDate)), // greter than operator 
+            $lt: new Date(new Date(endDate)) // Less than operator
+        }
+    }).sort({ date: 'asc' })  // sorted in ascending order
+
+     // quick search options. 
+    const one_year = articleSchema.find({ // 
+        date: {
+            $gte: new Date(new Date(2020)), // greter than operator 
+            $lt: new Date(new Date(2021)) // Less than operator
+        }
+    }).sort({ date: 'asc' })  // sorted in ascending order
+
+    const five_year = articleSchema.find({ // 
+        date: {
+            $gte: new Date(new Date(2017)), // greter than operator 
+            $lt: new Date(new Date(2021)) // Less than operator
+        }
+    }).sort({ date: 'asc' })  // sorted in ascending order
+
+    const ten_year = articleSchema.find({ // 
+        date: {
+            $gte: new Date(new Date(2011)), // greter than operator 
+            $lt: new Date(new Date(2021)) // Less than operator
+        }
+    }).sort({ date: 'asc' })  // sorted in ascending order
 }
 
 
- 
+
 
