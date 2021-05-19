efdca51011045e99465619caa892bc43fa57b023
Shahil <shkhan078@gmail.com>
2021-05-19 19:15:18 +1200

    Added backend unit tests

--- Tests/article.test.js
+++ Tests/article.test.js
@@ -21,34 +21,8 @@ describe('Articles', () => {
         });
     });
   });
-  describe('/POST Article', () => {
-    it('Posting an article', (done) => {
-      let article = {
-        title: "Trip to mars",
-        author: "John Doe",
-        year: 1999
-      }
-      chai.request(server)
-        .post('/articles')
-        .send(article)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          id = res.body._id
-          done();
-        });
-    });
-  });
-  describe(`/moderation/:ArticleId to remove newly created article so it doesnt get added to database`, () => {
-    it('deleting article', (done) => {
-        chai.request(server)
-            .delete(`/moderation/${id}`)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                done();
-            });
-    });
-});
+
+
+
 
 });
