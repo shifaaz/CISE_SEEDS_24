fb8de29495b4726a0685129bf8bdd1fcb845bced
Michell Mika <64630661+MichellMk2@users.noreply.github.com>
2021-05-19 17:58:21 +1200

    Cleared everything to create from scracth

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //mongoURI: "mongodb+srv://Hello:World@database.o473f.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    mongoURI: "mongodb+srv://shahil:xH2fIp0ecaBfSIN5@cluster0.1gqxl.mongodb.net/seedsDB?retryWrites=true&w=majority",
-
-    secretOrKey: "secret"
-};
